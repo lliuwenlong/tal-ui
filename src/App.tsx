9ec31321138115743f0c3e0c3a1c5c6e62097636
liuwenlong <18801295284@163.com>
2020-08-10 20:36:29 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import Button, {ButtonType, ButtonSize } from "./components/Button/button";
 function App() {
-  return (
-    <div className="App">
-      <a href="123">123123</a>
-    </div>
-  );
+    return (
+        <div>
+            <Button autoFocus>hello wowrd</Button>
+            <Button disabled>disabled</Button>
+            <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Pramary</Button>
+            <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
+            <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_black">Baidu Link</Button>
+            <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Disabled Link</Button>
+        </div>
+    );
 }
 
 export default App;
